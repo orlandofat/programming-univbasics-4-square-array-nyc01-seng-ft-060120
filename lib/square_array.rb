86aa8c68f32d47c3fd285bf9d96dcc206ab964a8
Orlando Challenger <orlandofat@gmail.com>
2020-05-15 02:41:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,6 @@
-def square_array(array)
- my_arr = [1, 2, 3]
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
+ef square_array(array)
+  array.each_with_object([]) { |i,arr| arr << i ** 2 }
 end
-end
+
+my_arr = [1, 2]
+p square_array(my_arr)
